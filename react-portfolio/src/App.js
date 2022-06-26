06ab563c8729f79fb8a1fc6adeb0658339a6411b
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-06-26 08:09:12 -0400

    fixing bugs

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -15,6 +15,21 @@ function App() {
     <Router>
     <div style={{ backgroundColor:"#A8A4A6" }}className="App">
       <Navigation></Navigation>
+      <div className="vh-100">
+            <div className="hero">
+                <img src={profile} alt='profile' className="heroPic" />
+                <div>
+                    <h1 className='heroHeader'>
+                        Hello there! My name is James Belk and this is my portfolio!
+                    </h1>
+                    <p className="myRed">
+                        I am a full stack developer with a wide variety of work experience and points of view that lead to a passion for an efficenet user experience. Please click on my "About Me" for a more in-depth history of who I am.
+                        <br />
+                        For my professional works pelase click on my Projects tab, and for a copy of my resume please click on the resume tab. If there are any questions at all please reach out to me via the contact tab!
+                    </p>
+                </div>
+            </div>
+        </div>
       <Routes>
         <Route path='' element={<Hero />} />
         <Route path='/about' element={<About />} />
