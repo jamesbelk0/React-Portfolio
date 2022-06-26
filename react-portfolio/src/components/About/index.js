1a41ef6b49e9b2038603dc3d60a01ac4d22c69bb
James Belk <78972994+jamesbelk0@users.noreply.github.com>
2022-06-26 09:25:55 -0400

    changed brand link

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -12,7 +12,10 @@ function About() {
                 <div>
                     <h1 className="text-center heroHeader">About Me</h1>
                     <p className="myRed">
-                        Front-end web developer leveraging a background in research and small business management with a passion to utilize this for a more intuitive user experience. My strengths are in my research skills as a former librarian, communication from leading a team of coaches in my own business, and ability to handle stress while still achieving the desired outcome. I recently earned a certificate in full stack development from the University of North Carolina - Charlotte focusing on developing skills in JavaScript, CSS, Node.Js and other responsive web design. For each project the goal is to engage my users for a pleasurable experience. I recently designed a user created blog with a team of 3 to allow users to create guides on whatever they see fit. I am excited to develop my skills and continue to build better experiences on the web.
+                        Front-end web developer leveraging a background in research and small business management with a passion to utilize this for a more intuitive user experience. My strengths are in my research skills as a former librarian, communication from leading a team of coaches in my own business, and ability to handle stress while still achieving the desired outcome.
+                        <br />
+                        <br />
+                         I recently earned a certificate in Full-Stack Development from the University of North Carolina - Charlotte focusing on developing skills in JavaScript, CSS, Node.Js and other responsive web design. For each project the goal is to engage my users for a enjoyable experience. I recently designed a user created blog with a team of 3 to allow users to create guides on whatever they see fit. I am excited to develop my skills and continue to build better experiences on the web.
                     </p>
                 </div>
             </div>
